449a0978b017ca0324234184cceb734bfa23c108
igorhasparyk <igor_hasparyk@hotmail.com>
2016-11-18 19:30:38 -0200

    Add files via upload

---
+++ Trab. M5 Banco de Dados/07_update_data.py
@@ -0,0 +1,26 @@
+# 07_update_data.py
+import sqlite3
+conn = sqlite3.connect('clientes.db')
+cursor = conn.cursor()
+id_cliente = 1
+novo_fone = '11-1000-2014'
+novo_criado_em = '2014-06-11'
+# alterando os dados da tabela
+cursor.execute("""
+UPDATE clientes
+SET fone = ?, criado_em = ?
+WHERE id = ?
+""", (novo_fone, novo_criado_em, id_cliente))
+conn.commit()
+print('Dados atualizados com sucesso.')
+conn.close()
+import sqlite3
+conn = sqlite3.connect('clientes.db')
+cursor = conn.cursor()
+# lendo os dados
+cursor.execute("""
+SELECT * FROM clientes;
+""")
+for linha in cursor.fetchall():
+    print(linha)
+conn.close()
